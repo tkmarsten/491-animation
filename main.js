6ec5f28206811de85669408f061c899e061832e9
tkmarsten <tkmarsten@gmail.com>
2021-01-19 16:00:10 -0800

    Please

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./naruto.png");
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
+	canvas.focus();
 	gameEngine.init(ctx);
-
+	gameEngine.addEntity(new Naruto(gameEngine));
 	gameEngine.start();
 });
---
+++ naruto.js
@@ -0,0 +1,24 @@
+class Naruto {
+    constructor(game) {
+        this.spritesheet = ASSET_MANAGER.getAsset("./naruto.png");
+
+        this.x = 100;
+        this.y = 100;
+
+        this.facing = 0; // 0 = right, 1 = left
+        this.game = game;
+        this.animations = [];
+
+        this.animations.push(new Animator(this.spritesheet, 25, 44, 32, 64, 0, 0, 0, false, false));
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 25, 44, 32, 64, 0, 0, 0, false, false);
+
+        this.animations[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, 1);
+    }
+
+    update() {
+
+    }
+}
